3f71480424529f49885604c661075694b0413583
Sandeep Pareek <psandeep01@gmail.com>
2025-02-27 03:57:24 +0530

    Upgrading to new Springboot version

--- user-service/src/main/java/com/sandeep/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/sandeep/userservice/UserServiceApplication.java
@@ -2,6 +2,7 @@ package com.sandeep.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
